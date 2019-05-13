2eab9da55e1a91da2748d917536a6deaa88b5abe
agustina <agustinariq@gmail.com>
2019-05-13 17:43:56 -0300

    CRUD completo

--- app/controllers/lodgings_controller.rb
+++ app/controllers/lodgings_controller.rb
@@ -1,7 +1,15 @@
 class LodgingsController < ApplicationController
 
+  def index
+    @lodgings = Lodging.all
+  end
+
   def new
     @lodging = Lodging.new
+  end
+
+  def edit
+    @lodging = Lodging.find(params[:id])
   end
 
   def create
@@ -9,8 +17,25 @@ class LodgingsController < ApplicationController
     redirect_to @lodging
   end
 
+  def update
+    @lodging = Lodging.find(params[:id])
+
+    if @lodging.update(lodging_params)
+      redirect_to @lodging
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @lodging = Lodging.find(params[:id])
+  end
+
+  def destroy
+    @lodging = Lodging.find(params[:id])
+    @lodging.destroy
+
+    redirect_to lodgings_path
   end
 
   private
