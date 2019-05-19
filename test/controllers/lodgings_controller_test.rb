24d225dadd6f7465ca82f1015cc52ffc862944f8
agustina <agustinariq@gmail.com>
2019-05-19 20:28:03 -0300

    Agrego test de controller

--- test/controllers/lodgings_controller_test.rb
+++ test/controllers/lodgings_controller_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class LodgingsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get lodgings_url
+    assert_response :success
+  end
+
+  test "should show lodging" do
+    lodging = Lodging.create
+    get lodgings_path(lodging)
+    assert_response :success
+  end
+
+  test "should destroy lodging" do
+    lodging = Lodging.create
+    assert_difference('Lodging.count', -1) do
+      delete lodging_path(lodging)
+    end
+  end
+
+  test "should update lodging" do
+    lodging = Lodging.create
+
+    updated = "Another province"
+
+    patch lodging_path(lodging), params: { lodging: { province: updated } }
+
+    lodging.reload
+    assert_equal updated, lodging.province
+  end
 end
